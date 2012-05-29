9334ee19dd57393172f481aada3d3ffdf4ef18c1
jakr <jk_reg@kristoferitsch.name>
2012-05-29 11:38:30 +0200

    Cleaned up code, wrote code to read from file.

--- code/DashboardLogWriter.php
+++ code/DashboardLogWriter.php
@@ -1,11 +1,12 @@
 <?php
 class DashboardLogWriter {
-	public static $DEFAULT_LOG_FILE = '';
-	/* Number of requests that are stored */
-	public static $REQUESTS = 10;
 	private $streamID;
 	private $logWriter;
-	private static $sessionKey = -1;
+	/* Number of requests that are stored */
+	public static $REQUESTS = 10;
+	public static $LOG_FILE_PATH = '';
+	private static $DEFAULT_LOG_FILE_PATH = '/../../debug.log';
+	private static $requestNumber = -1;
 	private static $messages = array();
 	private static $logWriters = array();
 	private static $SESSION_DATA_KEY = 'DEVELOPER_DASHBOARD_LOG_MESSAGES';
@@ -13,70 +14,121 @@ class DashboardLogWriter {
 	
 	public function __construct($streamID, $copyToFile=true){
 		$this->streamID = $streamID;
-		$logFilePath = self::getLogFilepath();
-		$this->logWriter = new Zend_Log_Writer_Stream($logFilePath);
+		$this->logWriter = new Zend_Log_Writer_Stream(self::getLogFilepath());
 		self::$logWriters[] = $this;
 	}
 
 	public function write($event){
 		$this->logWriter->write($event);
+		self::log(print_r($event,1), $this->streamID);
 	}
 
 	/**
+	 * Get the messages that have been logged during the current request.
 	 * 
-	 * Method 
-	 * This should be called from the error handler, 
+	 * This should be called from the global error handler, 
 	 *  to ensure that messages are output even if an error occured.
+	 * @return array the logged messages, one message per entry. 
 	 */
-	public static function getLogFileMessages($newerThan = 0){
-		fopen(self::getLogFilePath()){
-			
-		}
+	public static function getLogFileMessages(){
+		self::loadSavedMessagesFromSession();
+		return self::$messages[self::$requestNumber];
 	}
-
-	public static function getSavedMessages($newerThan = 0){
+	
+	/**
+	 * Get the messages that have been saved to the session.
+	 * If $newerThan is set, only entries from requests newer 
+	 *  than this request number are returned
+	 * 
+	 * @param int $newerThan 
+	 * @return array the messages as an array of 
+	 *   requestNumber=>array('message1', 'message2',...). 
+	 */
+	public static function getMessagesFromSession($newerThan = 0){
 		$ret = array();
 		self::loadSavedMessagesFromSession();
 		foreach(self::$messages as $key=>$messages){
 			if($key < $newerThan) continue;
-			$ret[] = $messages;
+			$ret[$key] = $messages;
 		}
 		return $ret;
 	}
-
-	private static function loadSavedMessagesFromSession(){
-		if(self::$sessionKey < 0){
-			Session::start();
-			self::$messages = Session::get(self::$SESSION_DATA_KEY);
-			if(self::$messages == null){
-				self::$messages = array();
-				self::$sessionKey = 0;
-			} else {
-				self::$sessionKey = count(array_keys(self::$messages));
-			}
-			self::$messages[self::$sessionKey] = array();
-		} 
-	}
-    
-	private static function getLogFilePath(){
-		$logFilePath = '';
-		if(self::$DEFAULT_LOG_FILE == ''){
-			 $logFilePath = dirname(__FILE__).'/../../debug.log';
-		} elseif (substr(self::$DEFAULT_LOG_FILE, 0, 2) == '..'){
-			 $logFilePath = dirname(__FILE__).'/'.self::$DEFAULT_LOG_FILE;
-		} else { //assume absolute path
-			 $logFilePath = self::$DEFAULT_LOG_FILE;
+	
+	/**
+	 * Read the log file from disk, starting at $offset.
+	 * 
+	 * @param int $offset
+	 * @return array with two keys:
+	 *  'last' is the offset of the last line
+	 *  'text' is an array of lines.
+	 */
+	public static function readLogFile($offset=-1){
+		$lines = array();
+		if($offset < 0) $offset = 0;
+		$file = fopen(self::getLogFilePath());
+		fseek($file, 0, SEEK_END);
+		$posEOF = ftell($file);
+		fseek($file, $offset);
+		while($line = fgets($file) !== false){
+			$lines[] = $line;
 		}
-		return $logFilePath;
+		fclose($file);
+		return array('last' => $posEOF, 'text' => $lines);	
 	}
-
-	public static function log($string, $streamID = 'DEFAULT', $timestamp = null){
+	
+	/**
+	 * Add $message to the log.
+	 * 
+	 * @param string $message 
+	 * @param string $streamID
+	 * @param int $timestamp
+	 */
+	public static function log($message, $streamID = 'DEFAULT',
+			$timestamp = null){
 		if($timestamp == null){
 			$timestamp = time();
 		}
 		self::loadSavedMessagesFromSession();
-		self::$messages[self::$sessionKey][] = "[$streamID] $timestamp $string";
+		self::$messages[self::$requestNumber][] = 
+			"[$streamID] $timestamp $message";
 		Session::set(self::$SESSION_DATA_KEY, self::$messages);
 		Session::save();
+	}
+
+	/**
+	 * The first time this function is called, it loads messages from past 
+	 *  requests that have been saved to the session into $messages 
+	 *  and sets $requestNumber.
+	 * Repeated calls to this function will not reset $requestNumber.
+	 * It will only display messages from requests in the current session.
+	 */
+	private static function loadSavedMessagesFromSession(){
+		if(self::$requestNumber >= 0){ //function was already called.
+			return;
+		}
+		Session::start();
+		self::$messages = Session::get(self::$SESSION_DATA_KEY);
+		if(self::$messages == null){
+			self::$messages = array();
+			self::$requestNumber = 0;
+		} else {
+			self::$requestNumber = count(array_keys(self::$messages));
+		}
+		self::$messages[self::$requestNumber] = array(); 
+	}
+    
+	/**
+	 * Figure out where our log files are stored.
+	 * 
+	 * @return string the log file path.
+	 */
+	private static function getLogFilePath(){
+		if(self::$LOG_FILE_PATH == ''){
+			 return dirname(__FILE__).self::$DEFAULT_LOG_FILE;
+		} elseif (substr(self::$LOG_FILE_PATH, 0, 2) == '..'){
+			 return dirname(__FILE__).'/'.self::$LOG_FILE_PATH;
+		} else { //assume absolute path
+			 return self::$LOG_FILE_PATH;
+		}
 	}
 }
--- code/DeveloperDashboard.php
+++ code/DeveloperDashboard.php
@@ -1,9 +1,9 @@
 <?php
 class DeveloperDashboard extends Controller {
 	public static $url_handlers = array(
-            'DeveloperDashboard/Log/$StartFrom' => 'GetLoggedData'
-            );
-	
+		'DeveloperDashboard/Log/$StartFrom' => 'GetLoggedData'
+	);
+
 	public function GetLoggedData($request, $StartFrom=0){
 		echo $StartFrom;
 		return DashboardLogWriter::getSavedMessages($StartFrom);

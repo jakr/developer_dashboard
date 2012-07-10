3cc5cc3aeadac480ecedadf19ae451d706dc3bf1
jakr <jk_reg@kristoferitsch.name>
2012-07-10 15:56:12 +0200

    Added information about current user.

--- _config.php
+++ _config.php
@@ -7,5 +7,10 @@ DashboardLogController::add_log_panel();
 //DEBUG: Call the log writer every time this file is loaded
 //This makes sure that every request generates at least one log message
 DashboardLog::log($_SERVER['REQUEST_METHOD'].' '.$_SERVER['REQUEST_URI'], 'Request');
+if(Member::currentUserID()){
+	DashboardLog::log('User ' . Member::currentUserID() . ' is logged in.', 'User');
+} else {
+	DashboardLog::log('No user logged in.', 'User');
+}
 DashboardLog::log('Hello World from Dashboard');
 DashboardLog::log('A Message in a different stream', 'SomeOtherStream');
